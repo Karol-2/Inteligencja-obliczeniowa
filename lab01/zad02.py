ba97f1a8e29e719248539353e919db903c071088
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-02-22 14:35:18 +0100

    Zrobione zadanie 2

--- lab01/zad02.py
+++ lab01/zad02.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 miasta = pd.read_csv("miasta.csv")
 print("zadanie 2 a")
@@ -7,23 +8,71 @@ print(miasta)
 print(miasta.values)
 print("zadanie 2 b")
 
-new_row = [2010,460,555,405]
-df = pd.DataFrame(miasta)
-df.loc[len(df)] = new_row
+new_row = [2010, 460, 555, 405]
+miasta.loc[10] = new_row
 
-print(df)
+print(miasta)
 print("zadanie 2 c")
-df.plot(kind='line',x="Rok",xticks=df.Rok,y="Gdansk",color="red", xlabel="Lata",
-        ylabel="Liczba ludności (w tys.)",
-        legend=False,title="Ludność w Gdańsku", style='.-'
-        )
-plt.gca().set_aspect('auto')
 
+plt.plot(miasta['Rok'], miasta['Gdansk'], 'ro-')
+
+years = np.arange(miasta['Rok'].min(), miasta['Rok'].max() + 1, 10)
+plt.xticks(years)
+
+plt.title('Ludność Gdańska')
+plt.xlabel('Rok')
+plt.ylabel('Liczba ludności [w tys.]')
+plt.grid()
 plt.show()
 
 print("zadanie 2 d")
+plt.plot(miasta['Rok'], miasta['Gdansk'], 'bo-', label='Gdańsk')
+plt.plot(miasta['Rok'], miasta['Poznan'], 'go-', label='Poznań')
+plt.plot(miasta['Rok'], miasta['Szczecin'], 'ro-', label='Szczecin')
 
-df.plot(kind='line',x="Rok",xticks=df.Rok, xlabel="Lata",
-        ylabel="Liczba ludności (w tys.)",
-        title="Ludność w miastach Polski", style='.-')
+plt.xticks(years)
+plt.title('Ludność w miastach Polski')
+plt.xlabel('Rok')
+plt.ylabel('Liczba ludności [w tys.]')
+plt.grid()
+
+plt.legend()
+
 plt.show()
+
+print("zadanie 2 e")
+kolumny = miasta[['Gdansk', 'Poznan', 'Szczecin']]
+
+
+def standaryzacja_kolumny(col):
+    return (col - col.mean()) / col.std()
+
+
+miasta_standaryzowane = kolumny.apply(standaryzacja_kolumny, axis=0)
+
+print(miasta_standaryzowane)
+
+srednia = miasta_standaryzowane.mean()
+odchylenie = miasta_standaryzowane.std()
+print("ŚREDNIA:")
+print(srednia)
+print("ODCHYLENIE:")
+print(odchylenie)
+
+print("zadanie 2 f")
+
+
+def normalizacja_kolumny(col):
+    return (col - col.min()) / (col.max() - col.min())
+
+
+miasta_normalizowane = kolumny.apply(normalizacja_kolumny, axis=0)
+
+print(miasta_normalizowane)
+
+min = miasta_normalizowane.min()
+max = miasta_normalizowane.max()
+print("MIN:")
+print(min)
+print("MAX:")
+print(max)
