c8de50581334f10998146cf8dfbabc741ef0be29
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-02-27 13:39:46 +0100

    lab 2 -  zadania

---
+++ lab02/partition_ga.py
@@ -0,0 +1,85 @@
+import pygad
+import numpy as np
+
+S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80]
+przedmioty = ["zegar", "obraz-pejzaż", "obraz-portret", "radio", "laptop", "lampka nocna", "srebren sztućce",
+              "porcelana", "figurka z brązu", "skórzana torebka", "odkurzacz"]
+wartosci = [100, 300, 200, 40, 500, 70, 100, 250, 300, 280, 300]
+wagi = [7, 7, 6, 2, 5, 6, 1, 3, 10, 3, 15]
+udzwig = 25
+n = len(wartosci)
+
+# definiujemy parametry chromosomu
+# geny to liczby: 0 lub 1
+gene_space = [0, 1]
+
+
+# definiujemy funkcję fitness
+def fitness_func(solution, solution_idx):
+    score = 0
+    score1 = 0
+    for i in range(len(wagi)):
+        score = score + np.sum(wagi[i]*gene_space[i])
+    if score >udzwig:
+        f = 0
+    else:
+        for i in range(len(wagi)):
+            score1 = score1 + np.sum(wartosci[i] * gene_space[i])
+        f = score1
+    return score1, score
+
+
+
+fitness_function = fitness_func
+
+# ile chromsomów w populacji
+# ile genow ma chromosom
+sol_per_pop = 10
+num_genes = len(wartosci)
+
+# ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
+# ile pokolen
+# ilu rodzicow zachowac (kilka procent)
+num_parents_mating = 5
+num_generations = 30
+keep_parents = 2
+
+# jaki typ selekcji rodzicow?
+# sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
+parent_selection_type = "sss"
+
+# w il =u punktach robic krzyzowanie?
+crossover_type = "single_point"
+
+# mutacja ma dzialac na ilu procent genow?
+# trzeba pamietac ile genow ma chromosom
+mutation_type = "random"
+mutation_percent_genes = 8
+
+# inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
+ga_instance = pygad.GA(gene_space=gene_space,
+                       num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_function,
+                       sol_per_pop=sol_per_pop,
+                       num_genes=num_genes,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       crossover_type=crossover_type,
+                       mutation_type=mutation_type,
+                       mutation_percent_genes=mutation_percent_genes)
+
+# uruchomienie algorytmu
+ga_instance.run()
+
+# podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Parameters of the best solution : {solution}".format(solution=solution))
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+
+# tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
+prediction = np.sum(S * solution)
+print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+
+# wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
+ga_instance.plot_fitness()
