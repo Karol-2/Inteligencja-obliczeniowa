251f6ac11042d7853c80425f6a4c31bacef2bffe
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-03-11 18:50:36 +0100

    zadanie 2 znajduje droge

--- lab03/zadanie2.py
+++ lab03/zadanie2.py
@@ -40,56 +40,62 @@ def fitness_func(solution, solution_idx):
     odwiedzone_pola = [position]
     karne_punkty = 0
     dobre_punkty = 0
-    cofanie_kara =10
-    sciana_kara= 10
+    cofanie_kara = 2
+    sciana_kara = 1
 
     for ruch in solution:
         if ruch == 0:  # lewo
-            if labirynt[position[0] - 1][position[1]] == "-":  # sprawdzenie czy nowe pole to nie ściana
-                if [position[0] - 1, position[1]] not in odwiedzone_pola:  # sprawdzenie czy się nie cofamy
-                    position = [position[0] - 1, position[1]]
-                    odwiedzone_pola.append(position)
-                    dobre_punkty +=5
+            kolejne_pole = labirynt[position[1]][position[0] - 1]
+            if kolejne_pole == "-":  # sprawdzenie czy nowe pole to nie ściana
+                position = [position[0] - 1, position[1]]  # jeśli nie ściana to wchodzimy
+                if position not in odwiedzone_pola:  # sprawdzenie czy już tu nie byliśmy wczesniej
+                    odwiedzone_pola.append(position)  # jeśli nowe pole dodajemy pozytywne pkt
+                    dobre_punkty += 5
                 else:
-                    karne_punkty += cofanie_kara  # kara za cofanie się
+                    karne_punkty += cofanie_kara  # kara za chodzenie po poprzednich polach
 
             else:  # kara jeśli chce uderzyć w ścianę
                 karne_punkty += sciana_kara
 
         elif ruch == 1:  # gora
-            if labirynt[position[0]][position[1] - 1] == "-":
-                if [position[0], position[1] - 1] not in odwiedzone_pola:
-                    position = [position[0], position[1] - 1]
+            kolejne_pole = labirynt[position[1] - 1][position[0]]
+            if kolejne_pole == "-":
+                position = [position[0], position[1] - 1]
+                if position not in odwiedzone_pola:
                     odwiedzone_pola.append(position)
-                    dobre_punkty +=5
+                    dobre_punkty += 5
                 else:
                     karne_punkty += cofanie_kara  # kara za cofanie się
             else:
                 karne_punkty += sciana_kara
-
+        # position = [1,1]
         elif ruch == 2:  # prawo
-            if labirynt[position[0] + 1][position[1]] == "-":
-                if [position[0] + 1, position[1]] not in odwiedzone_pola:
-                    position = [position[0] + 1, position[1]]
+            kolejne_pole = labirynt[position[1]][position[0] + 1]
+
+            if kolejne_pole == "-":
+                position = [position[0] + 1, position[1]]
+                if position not in odwiedzone_pola:
                     odwiedzone_pola.append(position)
-                    dobre_punkty +=5
+                    dobre_punkty += 5
                 else:
                     karne_punkty += cofanie_kara  # kara za cofanie się
+
             else:
                 karne_punkty += sciana_kara
 
         elif ruch == 3:  # dol
-            if labirynt[position[0]][position[1] + 1] == "-":
-                if [position[0], position[1] + 1] not in odwiedzone_pola:
-                    position = [position[0], position[1] + 1]
+            kolejne_pole = labirynt[position[1] + 1][position[0]]
+            if kolejne_pole == "-":
+                position = [position[0], position[1] + 1]
+                if position not in odwiedzone_pola:
                     odwiedzone_pola.append(position)
-                    dobre_punkty +=5
+                    dobre_punkty += 5
                 else:
                     karne_punkty += cofanie_kara  # kara za cofanie się
             else:
                 karne_punkty += sciana_kara
 
-    return odleglosc(position, exit_coord) - karne_punkty + dobre_punkty
+    return -odleglosc(position, exit_coord) - karne_punkty
 
 
 fitness_function = fitness_func
@@ -132,4 +138,80 @@ solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Parameters of the best solution: {solution}".format(solution=solution))
 print("Fitness value of the best solution  = {solution_fitness}".format(solution_fitness=solution_fitness))
 
+
+def rysowanie(solution, solution_idx):
+    position = start_coord
+    odwiedzone_pola = [position]
+    cofniecia = 0
+    uderzenia = 0
+
+    for ruch in solution:
+        if ruch == 0:  # lewo
+            kolejne_pole = labirynt[position[1]][position[0] - 1]
+            if kolejne_pole == "-":
+                position = [position[0] - 1, position[1]]
+                if position not in odwiedzone_pola:
+                    odwiedzone_pola.append(position)
+                else:
+                    cofniecia+=1
+            else:
+                uderzenia+=1
+
+        elif ruch == 1:  # gora
+            kolejne_pole = labirynt[position[1] - 1][position[0]]
+            if kolejne_pole == "-":
+                position = [position[0], position[1] - 1]
+                if position not in odwiedzone_pola:
+                    odwiedzone_pola.append(position)
+                else:
+                    cofniecia+=1
+            else:
+                uderzenia+=1
+        # position = [1,1]
+        elif ruch == 2:  # prawo
+            kolejne_pole = labirynt[position[1]][position[0] + 1]
+
+            if kolejne_pole == "-":
+                position = [position[0] + 1, position[1]]
+                if position not in odwiedzone_pola:
+                    odwiedzone_pola.append(position)
+                else:
+                    cofniecia+=1
+            else:
+                uderzenia+=1
+
+        elif ruch == 3:  # dol
+            kolejne_pole = labirynt[position[1] + 1][position[0]]
+            if kolejne_pole == "-":
+                position = [position[0], position[1] + 1]
+                if position not in odwiedzone_pola:
+                    odwiedzone_pola.append(position)
+                else:
+                    cofniecia+=1
+            else:
+                uderzenia+=1
+        if exit_coord in odwiedzone_pola:
+            print(f"ZNALEZIONO WYJSCIE!!, w {len(odwiedzone_pola)} krokach")
+            break
+    print(odwiedzone_pola)
+    print("uderzenia w ścianę: ",uderzenia)
+    print("cofnięcia: ",cofniecia)
+
+
+rysowanie(solution, 1)
 ga_instance.plot_fitness()
+
+'''
+Parameters of the best solution: [2. 2. 3. 3. 0. 0. 3. 3. 2. 3. 2. 3. 2. 1. 2. 1. 2. 1. 2. 3. 3. 2. 2. 3.
+ 2. 1. 3. 0. 3. 3.]
+Fitness value of the best solution  = -2
+[[1, 1], [2, 1], [3, 1], [3, 2], [3, 3], [2, 3], [1, 3], [1, 4], [1, 5], [2, 5], [2, 6], [3, 6], [3, 7], [4, 7], [4, 6], [5, 6], [5, 5], [6, 5], [6, 4], [7, 4], [7, 5], [7, 6], [8, 6], [9, 6], [9, 7], [10, 7], [10, 8], [10, 9], [10, 10]]
+
+Parameters of the best solution: [1. 1. 2. 2. 3. 2. 2. 1. 2. 2. 3. 3. 3. 0. 0. 3. 3. 2. 3. 3. 3. 0. 3. 3.
+ 2. 2. 2. 3. 2. 3.]
+Fitness value of the best solution  = -6
+
+Parameters of the best solution: [2. 3. 3. 2. 3. 2. 3. 3. 1. 2. 1. 2. 2. 3. 3. 3. 0. 3. 2. 3. 3. 3. 0. 3.
+ 3. 2. 1. 2. 2. 2.]
+Fitness value of the best solution  = -6
+'''
