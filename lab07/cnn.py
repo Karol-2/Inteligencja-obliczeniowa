c7c26a908e60fe4b70d3df49630d535ed3fc3364
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-04-24 11:03:57 +0200

    lab07

--- lab07/cnn.py
+++ lab07/cnn.py
@@ -13,6 +13,8 @@ def draw(img, x, y, color):
     img[x, y] = [color, color, color]
 
 
+
+
 # zamalowanie 4 pikseli w lewym górnym rogu
 draw(data, 5, 5, 100)
 draw(data, 6, 6, 100)
@@ -34,4 +36,41 @@ for i in range(128):
 
 # konwersja macierzy na obrazek i wyświetlenie
 plt.imshow(data, interpolation='nearest')
+plt.show()
+# ZADANIE B
+kernel = np.array([
+    [1, 0, -1],
+[1, 0, -1],
+[1, 0, -1]])
+
+def convolve2d(img, kernel, stride):
+    output_shape = ((img.shape[0] - kernel.shape[0]) // stride) + 1, ((img.shape[1] - kernel.shape[1]) // stride) + 1
+    output = np.zeros(output_shape)
+    kernel_size = kernel.shape[0]
+    padding = kernel_size // 2
+    img_padded = np.pad(img, padding, mode='constant', constant_values=0)
+    for i in range(0, img.shape[0]-kernel_size+1, stride):
+        for j in range(0, img.shape[1]-kernel_size+1, stride):
+            output[i//stride, j//stride] = np.sum(kernel * img_padded[i:i+kernel_size, j:j+kernel_size])
+    return output
+
+
+result = convolve2d(data[:, :, 0], kernel, stride=1)
+plt.imshow(result, cmap='gray')
+plt.show()
+# zADANIE C
+result = convolve2d(data[:, :, 0], kernel, stride=2)
+plt.imshow(result, cmap='gray')
+plt.show()
+
+# ZADANIE D
+kernel = np.array([
+    [1, 1,1],
+[0,0,0],
+[-1,-1,-1]])
+result = convolve2d(data[:, :, 0], kernel, stride=1)
+plt.imshow(result, cmap='gray')
+plt.show()
+result = convolve2d(data[:, :, 0], kernel, stride=2)
+plt.imshow(result, cmap='gray')
 plt.show()
