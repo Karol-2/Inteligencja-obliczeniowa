1b94a81b28dbd9820caea3089106b024baf34cbc
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-05-27 20:56:11 +0200

    Tlumaczenia na angielski tweetow

--- Projekt3 - Eurowizja/tlumaczenie.py
+++ Projekt3 - Eurowizja/tlumaczenie.py
@@ -1,16 +1,92 @@
 import pandas as pd
 from googletrans import Translator
 
-df = pd.read_csv('cyberpunk_after_release.csv')
-translator = Translator()
+
+# pip install googletrans==3.1.0a0
+
 
 def translate_to_english(text, lang):
+    translate_to_english.call_counter += 1
+    #print(translate_to_english.call_counter, lang)
     if lang == 'en':
         return text
+    elif lang == 'und' or lang == 'qme' or lang == 'qht':
+        return text
+    elif lang == 'zh':
+        translator = Translator(service_urls=['translate.googleapis.com'])
+        translation = translator.translate(text, dest='en', src='zh-tw')
+        return translation.text
+    elif lang == 'in':
+        translator = Translator(service_urls=['translate.googleapis.com'])
+        translation = translator.translate(text, dest='en', src='es')
+        return translation.text
     else:
-        translation = translator.translate(text, src=lang, dest='en')
+        translator = Translator(service_urls=['translate.googleapis.com'])
+        translation = translator.translate(text, dest='en', src=lang)
         return translation.text
 
-df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
 
-df.to_csv('ENG_after_release.csv', index=False)
+translate_to_english.call_counter = 0
+
+def q1():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_first_move.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_first_move.csv', index=False)
+
+
+def q2():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_second_move.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_second_move.csv', index=False)
+
+
+def q3():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_third_move.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_third_move.csv', index=False)
+
+
+def q4():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_after_release.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_after_release.csv', index=False)
+
+
+def q5():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_before_release.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_before_release.csv', index=False)
+
+
+def q6():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_first_trailer.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_first_trailer.csv', index=False)
+
+
+def q7():
+    translate_to_english.call_counter = 0
+    df = pd.read_csv('cyberpunk_first_gameplay.csv')
+    df['Content'] = df.apply(lambda row: translate_to_english(row['Content'], row['Lang']), axis=1)
+
+    df.to_csv('ENG_first_gameplay.csv', index=False)
+
+
+q2()
+q3()
+q4()
+q5()
+q6()
+q7()
